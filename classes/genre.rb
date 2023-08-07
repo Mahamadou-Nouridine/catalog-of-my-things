c05a962d76ba8bb5c0ca3264f829806a9619a07a
Mahamadou-Nouridine <98597449+Mahamadou-Nouridine@users.noreply.github.com>
2023-08-07 23:16:39 +0100

    Create genre class and implement add_item method

---
+++ classes/genre.rb
@@ -0,0 +1,13 @@
+class Genre
+  attr_accessor :name, :items
+  def initialize(name)
+    @id= rand(0..1000)
+    @name=name
+    @items=[]
+  end
+
+  def add_item(item)
+    @items << item unless @items.include?(item)
+    item.add_genre(self)
+  end
+end
