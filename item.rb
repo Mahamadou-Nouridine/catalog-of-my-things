7da5cbc0bf27c5357d3fc992278d79ce7dbd5403
danielsafari143 <danielsafari143@gmail.com>
2023-08-07 16:28:32 +0200

    FIX_ERRORS

--- item.rb
+++ item.rb
@@ -1,5 +1,3 @@
-require 'date'
-
 class Item
   attr_accessor :publish_date, :archived, :label, :author, :genre, :source
   attr_reader :id
@@ -10,8 +8,12 @@ class Item
     @archived = archived
   end
 
-  def add_source(source)
-    @source = source
+  def add_genre(genre)
+    @genre = genre
+  end
+
+  def add_author(author)
+    @author = author
   end
 
   def can_be_archived?
--- main.rb
+++ main.rb
@@ -1,18 +1,14 @@
-option = {
-
-}
-
 def main
   puts "Welcom to Catalog of my things applpication\n\n"
   loop do
-    puts "Select an option to continue"
+    puts 'Select an option to continue'
     user_option = gets.chomp.to_s
     case user_option
     when '10'
-      puts "Bye bye, see you again!"
+      puts 'Bye bye, see you again!'
       exit
     else
-      puts "Invalid option, please select again a number between 1 and 10"
+      puts 'Invalid option, please select again a number between 1 and 10'
     end
   end
 end
