54aa7269d9195fab8b849e0f65e0116826f72aab
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-08-08 19:01:13 +0100

    Fix linters errors

--- Rspec/specs/genre_spec.rb
+++ Rspec/specs/genre_spec.rb
@@ -2,20 +2,20 @@ require_relative '../../classes/genre'
 require_relative '../../classes/item'
 
 describe Genre do
-    it 'Create Hip-hop genre' do
-      genre = Genre.new('Hip-hop')
+  it 'Create Hip-hop genre' do
+    genre = Genre.new('Hip-hop')
 
-      expect(genre.name).to eq('Hip-hop')
-      expect(genre).to be_instance_of(Genre)
-    end
+    expect(genre.name).to eq('Hip-hop')
+    expect(genre).to be_instance_of(Genre)
+  end
 
-    it "should add an item to the genre's items array" do
-      genre = Genre.new('Hip-hop')
-      item = Item.new('2022-10-10')
+  it "should add an item to the genre's items array" do
+    genre = Genre.new('Hip-hop')
+    item = Item.new('2022-10-10')
 
-      genre.add_item(item)
+    genre.add_item(item)
 
-      expect(genre.items.length).to eq(1)
-      expect(genre.items[0].publish_date).to eq('2022-10-10')
-    end
+    expect(genre.items.length).to eq(1)
+    expect(genre.items[0].publish_date).to eq('2022-10-10')
+  end
 end
--- Rspec/specs/music_album_spec.rb
+++ Rspec/specs/music_album_spec.rb
@@ -1,16 +1,16 @@
 require_relative '../../classes/music_album'
 
 describe MusicAlbum do
-    it 'Create Music Album' do
-      music_album = MusicAlbum.new('2023-03-03', on_spotify: true)
+  it 'Create Music Album' do
+    music_album = MusicAlbum.new('2023-03-03', on_spotify: true)
 
-      expect(music_album.on_spotify).to eq(true)
-      expect(music_album).to be_instance_of(MusicAlbum)
-    end
+    expect(music_album.on_spotify).to eq(true)
+    expect(music_album).to be_instance_of(MusicAlbum)
+  end
 
-    it 'should return "false" for archived' do
-      music_album = MusicAlbum.new('2023-03-12', on_spotify: true)
+  it 'should return "false" for archived' do
+    music_album = MusicAlbum.new('2023-03-12', on_spotify: true)
 
-      expect(music_album.archived).to eq(false)
-    end
+    expect(music_album.archived).to eq(false)
+  end
 end
