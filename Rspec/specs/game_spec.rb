cbec40101707cd61e24e8bb5e0858987ba081a6b
danielsafari143 <danielsafari143@gmail.com>
2023-08-09 00:51:06 +0200

    REFACTOR

--- Rspec/specs/game_spec.rb
+++ Rspec/specs/game_spec.rb
@@ -2,18 +2,21 @@ require_relative '../../classes/game'
 
 # frozen_string_literal: true
 
-describe 'Test the game class' do
-  context 'can_be_archieved' do
-    it 'should the the can_be_archieved method' do
-      game = Game.new('22-2-2023', true, '22-2-2023')
+describe Game do
+  context 'can be archived' do
+    it 'is true if the game was released more than 10 years ago and the last played date is more than 2 years ago' do
+      game = Game.new('22-2-2010', true, '22-2-2020')
+      expect(game.can_be_archived?).to eq(true)
+    end
+
+    it 'is false if the game was released less than 10 years ago and the last played date is more than 2 years ago' do
+      game = Game.new('22-2-2023', true, '22-2-2020')
       expect(game.can_be_archived?).to eq(false)
     end
-  end
 
-  context 'can_be_archieved' do
-    it 'should the the can_be_archieved method' do
-      game = Game.new('22-2-2010', true, '22-2-2010')
-      expect(game.can_be_archived?).to eq(true)
+    it 'is false if the game was released more than 10 years ago and the last played date is less than 2 years ago' do
+      game = Game.new('22-2-2010', true, '22-2-2021')
+      expect(game.can_be_archived?).to eq(false)
     end
   end
 end
