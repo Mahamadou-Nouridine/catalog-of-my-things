e970cc983c8b3e982ffe691a7f66a7ed7bd0b085
danielsafari143 <danielsafari143@gmail.com>
2023-08-09 00:13:43 +0200

    ADD GAME TEST

---
+++ Rspec/specs/game_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../../classes/game'
+
+# frozen_string_literal: true
+
+describe 'Test the game class' do
+  context 'can_be_archieved' do
+    it 'should the the can_be_archieved method' do
+      game = Game.new('22-2-2023', true, '22-2-2023')
+      expect(game.can_be_archived?).to eq(false)
+    end
+  end
+end
