12364e1f8f52555a5c38b201d1ee9344f72cfd9b
danielsafari143 <danielsafari143@gmail.com>
2023-08-09 01:16:48 +0200

    ADD TEST FOR AUTHOR

--- Rspec/specs/author_spec.rb
+++ Rspec/specs/author_spec.rb
@@ -1,12 +1,26 @@
 require_relative '../../classes/author'
+require_relative '../../classes/item'
 
 # frozen_string_literal: true
 
-describe Author  do
-    context 'add item' do
-        it 'author' do
-            game = Author.new('John', 'Doe')
-            expect(game.add_item).to eq(true)
-        end
+describe Author do
+  context 'add item' do
+    it 'author' do
+      item = Item.new('22-2-2023')
+
+      author = Author.new('John', 'Doe')
+
+      expect(author.add_item(item)[0]).to eq(item)
     end
+
+    it 'author' do
+      item = Item.new('2-2-2023')
+      item2 = Item.new('21-1-2010')
+
+      author = Author.new('Laurent', 'Zai')
+      author.add_item(item2)
+
+      expect(author.add_item(item)[1]).to eq(item)
+    end
+  end
 end
