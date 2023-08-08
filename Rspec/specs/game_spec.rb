195cc5cf929d7a3cc23d2addea983eb729054b23
danielsafari143 <danielsafari143@gmail.com>
2023-08-09 00:19:34 +0200

    REFACTOR

--- Rspec/specs/game_spec.rb
+++ Rspec/specs/game_spec.rb
@@ -9,4 +9,11 @@ describe 'Test the game class' do
       expect(game.can_be_archived?).to eq(false)
     end
   end
+
+  context 'can_be_archieved' do
+    it 'should the the can_be_archieved method' do
+      game = Game.new('22-2-2010', true, '22-2-2010')
+      expect(game.can_be_archived?).to eq(true)
+    end
+  end
 end
