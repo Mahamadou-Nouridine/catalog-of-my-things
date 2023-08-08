cd84797e8d46f9952147a411e9b5d368b1d1791f
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-08-08 05:20:37 +0000

    Modify files

--- label.rb
+++ label.rb
@@ -1,9 +1,9 @@
 class Label < Item
   attr_accessor :items, :name, :id
 
-  def initialize(name, title, color)
+  def initialize(title, title, color)
     super()
-    @id = id = Random.rand(1..1000)
+    @id = Random.rand(1..1000)
     @title = title
     @color = color
     @items = []
