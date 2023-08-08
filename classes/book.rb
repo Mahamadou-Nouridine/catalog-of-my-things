6a5c42617284bff78458752d1ca76de370332b30
Mahamadou-Nouridine <98597449+Mahamadou-Nouridine@users.noreply.github.com>
2023-08-08 15:03:28 +0100

    Resolve linters issues

--- classes/book.rb
+++ classes/book.rb
@@ -2,7 +2,7 @@ class Book < Item
   attr_accessor :publisher, :cover_state
 
   def initialize(publish_date, publisher, cover_state, archived: false)
-    super(publish_date, archived:)
+    super(publish_date, archived: archived)
     @publisher = publisher
     @cover_state = cover_state
   end
