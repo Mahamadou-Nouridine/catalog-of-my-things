fef619266c1a3ffc579b0ff99c6effa06c694319
Mahamadou-Nouridine <98597449+Mahamadou-Nouridine@users.noreply.github.com>
2023-08-08 12:23:35 +0100

    Restructure list Items and preserve data

--- main.rb
+++ main.rb
@@ -4,6 +4,8 @@ OPTIONS = {
   '1' => 'add_music_album',
   '2' => 'List all Music albums',
   '3' => 'List all genres',
+  '4' => 'List All Authors',
+  '5' => 'List All Games',
   '10' => 'Quit'
 }.freeze
 
@@ -21,6 +23,10 @@ def main
       app.list_music_albums
     when '3'
       app.list_genres
+    when '4'
+      app.list_authors
+    when '5'
+      app.list_games
     when '10'
       puts 'Bye bye, see you again!'
       exit
