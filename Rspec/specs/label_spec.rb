fc693fd4d9b54caba1069c2be53617e6cfd07ba5
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-08-09 12:14:35 +0000

    Add label test file

---
+++ Rspec/specs/label_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../../classes/label'
+
+describe Label do
+  describe '#initialize' do
+    it 'sets id, title, color, and initializes items' do
+      subject = Label.new('Test title','Green')
+      expect(subject).to be_instance_of(Label)
+      expect(subject.object_to_hash['items']).to eq([])
+      expect(subject.object_to_hash['color']).to eq('Green')
+      #expect(subject.object_to_hash['title']).to eq('Test title')
+    end
+  end
+end
