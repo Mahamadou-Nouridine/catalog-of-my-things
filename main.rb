064b6310bcd26a92260ac06a27a415822ef36aa2
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-08-09 06:02:58 +0000

    Modify main.rb file

--- main.rb
+++ main.rb
@@ -15,37 +15,25 @@ OPTIONS = {
 
 def main
   app = App.new
-  puts '=================================================='
   puts "\nWelcome to Catalog of my things applpication\n\n"
-  puts '=================================================='
   loop do
     puts 'Select an option to continue'
     OPTIONS.each { |index, string| puts "#{index} - #{string}" }
     user_option = gets.chomp.to_s
     case user_option
-    when '1'
-      app.list_books
-    when '2'
-      app.list_music_albums
-    when '3'
-      app.list_games
-    when '4'
-      app.list_genres
-    when '5'
-       # Implement label
-    when '6'
-      app.list_authors
-    when '7'
-      app.add_book
-    when '8'
-      app.add_music_album
-    when '9'
-      app.add_game
+    when '1' then app.list_books
+    when '2' then app.list_music_albums
+    when '3' then app.list_games
+    when '4' then app.list_genres
+    when '6' then app.list_authors
+    when '7' then app.add_book
+    when '8' then app.add_music_album
+    when '9' then app.add_game
     when '10'
       puts 'Bye bye, see you again!'
       exit
     else
-      puts 'Invalid option, please select again a number between 1 and 10'
+      puts 'Invalid option, please select a number between 1 and 10'
     end
   end
 end
