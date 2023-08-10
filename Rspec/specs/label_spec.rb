145b9cccc3a5c024ee17aee0512a532b328c88a6
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-08-10 02:49:32 +0000

    Remove linters error

--- Rspec/specs/label_spec.rb
+++ Rspec/specs/label_spec.rb
@@ -3,7 +3,7 @@ require_relative '../../classes/label'
 describe Label do
   describe '#initialize' do
     it 'sets id, title, color, and initializes items' do
-      subject = Label.new('Test title','Green')
+      subject = Label.new('Test title', 'Green')
       expect(subject).to be_instance_of(Label)
       expect(subject.object_to_hash['items']).to eq([])
       expect(subject.object_to_hash['color']).to eq('Green')
